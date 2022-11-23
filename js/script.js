e57534cdf8b93178e82a6c2a6d7a62b4f2bdbe71
Jasurbek999 <jasurbeksaribayev99@gmail.com>
2022-11-23 22:57:28 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+//alert("Hello Javascript");
+//const place = confirm ("Are you here?");
+//console.log (place);
+
+//const currentPlace = prompt("Where are you?" , "12");
+//console.log (typeof currentPlace);
+
+//const currentPlace= prompt ("How old are you?", "18");
+//console.log (typeof currentPlace);
+
+//const favouriteColor = [];
+
+//vouriteColor[0]= prompt ("What`s your favourite color #1", "");
+//favouriteColor[1]= prompt ("What`s your favourite color #1", "");
+//avouriteColor[2]= prompt ("What`s your favourite color #1", "");
+
+//console.log (favouriteColor);
+
+//const channel =prompt ("What`s your favourite youtuber?");
+
+//console.log (`https://youtube.com/${channel}`);
+
+//const user = "Jasurbek";
+//const job = "Javascript developer";
+//console.log (`User name is ${user} he is ${job}`);
+
+
+//Foydali qollanma o'qib chiqing:
+
+//JavaScript-da interpolatsiya shunga o'xshash tarzda ishlaydi. Satrda $ {...} konstruktsiyasi yaratilgan bo'lib, uning ichiga istalgan o'zgaruvchini yoki ifodani joylashtirish mumkin:
+
+//console.log ("Jasurbek" + "Saribayev");
+
+///let incr =10;
+//let dencr =10;
+
+
+//console.log (incr++);
+//console.log (dencr--);
+//console.log (incr++);
+//onsole.log (dencr--);
+//console.log (incr);
+//console.log (dencr);
+
+//console.log(5 % 3);
+//console.log(5*5 ==="25");
+
+//     && va  bu || esa yoki degan ma`noni bildiradi
+
+//const isAge = true;
+//const isMarried =true;
+//const isChacked =false;
+//console.log (isAge ||isMarried ||isChacked);
+
+console. log(2+2*2 === 8);
